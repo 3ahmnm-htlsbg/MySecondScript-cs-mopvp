d0c6b09e9da9fc0896246303b00968e2f74743f9
mopvp <moritz.niedermaier01@htl-salzburg.ac.at>
2019-11-05 14:42:29 +0100

    update README

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start()Debug.Log(“Hello World at home“);
     {
-        Debug.Log(“Hello World at home“);
+
     }
 
     // Update is called once per frame
